e780bce3eb3c0ce949433d280642c4bebd98bfa6
hirohana <hirohanahaba@gmail.com>
2021-08-10 12:45:15 +0900

    rist4-11 Initialization

--- src/App.js
+++ src/App.js
@@ -11,13 +11,21 @@ const App = () => {
     setVal(event.target.value)
   }
 
-  const doAction = () => {
+  // const doAction = () => {
+  //   let res = <div>
+  //     <p>軽減税率(8%) : {tax1} 円</p>
+  //     <p>軽減税率(10%) : {tax2} 円</p>
+  //   </div>
+  //   setMsg(res)
+  // }
+
+  useEffect(() => {
     let res = <div>
       <p>軽減税率(8%) : {tax1} 円</p>
       <p>軽減税率(10%) : {tax2} 円</p>
     </div>
     setMsg(res)
-  }
+  }, [tax1, tax2])
 
   useEffect(() => {
     setTax1(Math.floor(val * 1.08))
@@ -38,8 +46,8 @@ const App = () => {
           <input type="number" className="form-control"
             onChange={doChange} />
         </div>
-        <button className="btn btn-primary"
-          onClick={doAction}>Calc</button>
+        {/* <button className="btn btn-primary"
+          onClick={doAction}>Calc</button> */}
       </div>
     </div>
   )
